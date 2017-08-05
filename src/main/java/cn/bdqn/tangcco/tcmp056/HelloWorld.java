fc7df93621f9c6f0aacf398b7a7a5dfbab651a37
shangdi1122 <1399070052@qq.com>
2017-08-05 12:48:08 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/HelloWorld.java
@@ -0,0 +1,10 @@
+package cn.bdqn.tangcco.tcmp056;
+
+/**
+ * Created by THINKPAD on 2017/8/5.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello,world!");
+    }
+}
---
+++ src/main/java/cn/bdqn/tangcco/tcmp056/HelloWorld1.java
@@ -0,0 +1,16 @@
+package cn.bdqn.tangcco.tcmp056;
+
+import org.junit.Test;
+
+/**
+ * Created by THINKPAD on 2017/8/5.
+ */
+public class HelloWorld1 {
+
+@Test
+    public void print() {
+    System.out.println("只能存在三个元素x,y,z，否则无法导入");
+}
+
+
+}
